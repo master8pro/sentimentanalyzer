bde8822c549949612a845bcceab8dade6279a13a
master8pro <51298008+master8pro@users.noreply.github.com>
2025-07-31 21:37:30 +0800

    Update fetch_tweets.py

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -2,14 +2,14 @@ import requests
 import pandas as pd
 import os
 
-BEARER_TOKEN = os.environ.get("TWITTER_BEARER_TOKEN")  # Set this in Streamlit secrets
+BEARER_TOKEN = os.environ.get("AAAAAAAAAAAAAAAAAAAAAK463QEAAAAAn4pu7PcZ6KLHBr86kGZWajmGjto%3DwFjGA3oRi5vbsIUjxCjDMXPeakk5kYqfDt7XPHxXZJHLZWErd6")  # Set this in Streamlit secrets
 SEARCH_QUERY = "Sabah OR PRN Sabah OR WARISAN OR GRS OR PH OR BN"
 MAX_RESULTS = 50
 
 def fetch_tweets():
     url = f"https://api.twitter.com/2/tweets/search/recent?query={SEARCH_QUERY}&max_results={MAX_RESULTS}&tweet.fields=created_at,text"
     headers = {
-        "Authorization": f"Bearer {BEARER_TOKEN}"
+        "Authorization": f"Bearer {AAAAAAAAAAAAAAAAAAAAAK463QEAAAAAn4pu7PcZ6KLHBr86kGZWajmGjto%3DwFjGA3oRi5vbsIUjxCjDMXPeakk5kYqfDt7XPHxXZJHLZWErd6}"
     }
 
     response = requests.get(url, headers=headers)
